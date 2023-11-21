3478141102a2d9a285601f4b6921278445410c80
Moritz Waldleben <moritz.waldleben@gmail.com>
2023-11-21 09:19:33 +0100

    change plugin manager from packer to lazy

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,129 +1,108 @@
-  local fn = vim.fn
+local fn = vim.fn
 
--- automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+-- install plugin manager
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
 
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-	return
+  print("Error in require for lazy")
 end
 
--- packer with popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float()
-		end,
-	},
-})
+lazy.setup({
+  -- general
+  "wbthomason/packer.nvim",
+  "nvim-lua/plenary.nvim",
+  "nvim-tree/nvim-web-devicons",
 
-return packer.startup(function(use)
- 	-- general
-	use "wbthomason/packer.nvim"
-	use "nvim-lua/plenary.nvim"
-	use "kyazdani42/nvim-web-devicons"
+  -- autopairs
+  "windwp/nvim-autopairs",
 
- 	-- autopairs
-	use "windwp/nvim-autopairs"
+  -- colorscheme
+  "catppuccin/nvim",
 
- 	-- bufferline
-	use "akinsho/bufferline.nvim"
-
-	-- colorscheme
-	use "folke/tokyonight.nvim"
-  use "EdenEast/nightfox.nvim"
-  use "rebelot/kanagawa.nvim"
+   -- cokeline 
+  "willothy/nvim-cokeline",
 
   -- comment
-  use "numToStr/Comment.nvim"
+  "numToStr/Comment.nvim",
 
-  -- copilot
-  use "zbirenbaum/copilot.lua"
-
-	-- cmp plugins
-	use "hrsh7th/nvim-cmp"
-	use "hrsh7th/cmp-buffer"
-	use "hrsh7th/cmp-path"
-	use "saadparwaiz1/cmp_luasnip"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-  use "zbirenbaum/copilot-cmp"
+  -- cmp plugins
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "saadparwaiz1/cmp_luasnip",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lua",
 
   -- gitsigns
-  use "lewis6991/gitsigns.nvim"
+  "lewis6991/gitsigns.nvim",
 
   -- indent-blankline
-  use "lukas-reineke/indent-blankline.nvim"
+  "lukas-reineke/indent-blankline.nvim",
+
+  -- julia-vim 
+  "JuliaEditorSupport/julia-vim",
 
   -- lualine
-  use "nvim-lualine/lualine.nvim"
+  "nvim-lualine/lualine.nvim",
 
   -- lsp
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
-  use "neovim/nvim-lspconfig"
-  use "jose-elias-alvarez/null-ls.nvim"
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  "nvimtools/none-ls.nvim",
 
- 	-- nvimtree
-	use "kyazdani42/nvim-tree.lua"
+  -- nvimtree
+  "kyazdani42/nvim-tree.lua",
 
-  -- nvim-dap
-  use "mfussenegger/nvim-dap"
-  use "jayp0521/mason-nvim-dap.nvim"
-  use "rcarriga/nvim-dap-ui"
+  -- orgmode
+  "nvim-orgmode/orgmode",
 
   -- project
-  use "ahmedkhalf/project.nvim"
+  "ahmedkhalf/project.nvim",
 
-	-- snippets
-	use "L3MON4D3/LuaSnip"
-	use "rafamadriz/friendly-snippets"
+  -- snippets
+  "L3MON4D3/LuaSnip",
 
   -- smart-splits
-  use "mrjones2014/smart-splits.nvim"
+  "mrjones2014/smart-splits.nvim",
 
-	-- telescope
-	use "nvim-telescope/telescope.nvim"
+  -- telescope
+  "nvim-telescope/telescope.nvim",
+
+  -- transparent
+  "xiyaowong/transparent.nvim",
 
   -- tpope plugins
-  use "tpope/vim-fugitive"
-  use "tpope/vim-repeat"
-  use "tpope/vim-surround"
-  use "tpope/vim-unimpaired"
+  "tpope/vim-fugitive",
+  "tpope/vim-repeat",
+  "tpope/vim-surround",
+  "tpope/vim-unimpaired",
 
   -- toggleterm
-  use "akinsho/toggleterm.nvim"
+  "akinsho/toggleterm.nvim",
 
-	-- treesitter
-	use "nvim-treesitter/nvim-treesitter"
+  -- treesitter
+  "nvim-treesitter/nvim-treesitter",
 
   -- trouble
-  use "folke/trouble.nvim"
+  "folke/trouble.nvim",
+
+  -- vimtex
+  "lervag/vimtex",
 
   -- which-key
-  use "folke/which-key.nvim"
-
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+  "folke/which-key.nvim",
+})
