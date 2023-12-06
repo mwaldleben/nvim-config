b5a3699439466c11a4a06d4649a6a2036769a47a
Moritz Waldleben <moritz@waldleben.ch>
2023-12-06 16:10:53 +0100

    change toggleterm terminal to horizontal

--- lua/moritz/plugins/toggleterm.lua
+++ lua/moritz/plugins/toggleterm.lua
@@ -1,8 +1,22 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  opts = {
-    open_mapping = [[<c-\>]],
-    direction = "float",
-  },
+  config = function()
+    local toggleterm = require("toggleterm")
+
+    toggleterm.setup({
+      size = 25,
+      open_mapping = [[<c-\>]],
+      direction = "horizontal",
+      shading_factor = "-20",
+    })
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+    end
+
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+  end,
 }
