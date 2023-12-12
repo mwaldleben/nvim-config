80031dec4b5510a0a510c2cb251387eb275fdfcb
Moritz Waldleben <moritz@waldleben.ch>
2023-12-12 16:14:45 +0100

    nvim-tree: don't hijack netrw, is needed for spell

--- lua/moritz/plugins/nvim-tree.lua
+++ lua/moritz/plugins/nvim-tree.lua
@@ -2,11 +2,7 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
     require("nvim-tree").setup({
-      sync_root_with_cwd = true,
       respect_buf_cwd = true,
       update_focused_file = {
         enable = true,
