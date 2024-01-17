af82c90257cfcddbdbde0692f7b6dfd3a8461ce5
Moritz Waldleben <moritz@waldleben.ch>
2024-01-17 10:24:06 +0100

    add french to spell checking

--- lua/moritz/core/autocmd.lua
+++ lua/moritz/core/autocmd.lua
@@ -10,4 +10,4 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 vim.cmd("autocmd FileType qf wincmd J")
 
-vim.cmd("autocmd FileType latex,tex,markdown,md,mail setlocal spell spelllang=de_ch,en_us")
+vim.cmd("autocmd FileType latex,tex,markdown,md,mail setlocal spell spelllang=de_ch,en_us,fr")
