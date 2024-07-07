2f1aae601e039dcf6ad3947c5a994e4acf1dc316
Moritz Waldleben <moritz@waldleben.ch>
2024-07-07 22:26:02 +0200

    trouble: update keybindings

--- lua/moritz/plugins/trouble.lua
+++ lua/moritz/plugins/trouble.lua
@@ -1,16 +1,37 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("trouble").setup()
-
-    local keymap = vim.keymap
-
-    keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>")
-    keymap.set("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
-    keymap.set("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>")
-    keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>")
-    keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>")
-    keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>")
-  end,
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
