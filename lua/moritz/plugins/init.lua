5dffefed56f68b5f8dc67a71e336833ec8b29ac7
Moritz Waldleben <moritz@waldleben.ch>
2025-11-02 23:49:59 +0100

    Remove some unused plugins

--- lua/moritz/plugins/init.lua
+++ lua/moritz/plugins/init.lua
@@ -1,16 +1,6 @@
 -- plugins where no setup is required
 return {
-  "nvim-lua/plenary.nvim",
-
-  "xiyaowong/transparent.nvim",
-
   "tpope/vim-surround",
 
   "tpope/vim-repeat",
-
-  "JuliaEditorSupport/julia-vim",
-
-  "norcalli/nvim-colorizer.lua",
-
-  "renerocksai/calendar-vim",
 }
