45ee3a6d93f69bb9197110d100c6e7f3915fd800
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 16:17:44 +0100

    treesitter: move config to opt table

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,10 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  config = function()
-    local treesitter = require('nvim-treesitter.configs')
-
-    treesitter.setup({
-      ensure_installed = {
-        'lua',
-        'c',
-        'cpp',
-        'fortran',
-        'latex',
-        'make',
-        'html',
-        'css',
-        'astro',
-        'typescript',
-        'markdown',
-      },
-      ignore_install = { '' },
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+  opts = {
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+  },
 }
