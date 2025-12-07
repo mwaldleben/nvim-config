7827a261f0a770a2b160f3d97dec3043bad9dab6
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 15:18:19 +0100

    telescope: Refactor and add project.nvim keybinding

--- lua/moritz/plugins/nvim-tree.lua
+++ lua/moritz/plugins/nvim-tree.lua
@@ -1,14 +1,16 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("nvim-tree").setup({
-      respect_buf_cwd = true,
-      update_focused_file = {
-        enable = true,
-        update_root = true,
-      },
-    })
-    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
-  end,
+  'nvim-tree/nvim-tree.lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    sync_root_with_cwd = true,
+    update_focused_file = {
+      enable = true,
+    },
+    view = {
+      width = {}, -- dynamically resize window based on longest line
+    },
+  },
+  keys = {
+    { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Open file [E]xplorer' },
+  },
 }
---
+++ lua/moritz/plugins/project.lua
@@ -0,0 +1,7 @@
+return {
+  'DrKJeff16/project.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {},
+}
