260cde12b32c9bd77592a1a055fd21b0a0e58b5f
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 15:19:09 +0100

    Add oil.nvim plugin

---
+++ lua/moritz/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {},
+  lazy = false,
+  keys = {
+    { '-', '<cmd>Oil --float<cr>', desc = '[-] Open parent directory' },
+  },
+}
