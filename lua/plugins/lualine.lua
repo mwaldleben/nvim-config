d68c871bb9c928e8bd71515c8c807a28658cddb1
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 16:06:49 +0100

    lualine: move to opts table for config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,17 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local lualine = require('lualine')
-
-    lualine.setup({
-      options = {
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = { 'NvimTree' },
+  opts = {
+    options = {
+      component_separators = {
+        left = '',
+        right = '',
       },
-    })
-  end,
+      section_separators = {
+        left = '',
+        right = '',
+      },
+      disabled_filetypes = { 'NvimTree' },
+    },
+  },
 }
