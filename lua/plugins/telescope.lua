2d3b560e06ac3c1aa4fa87b50389c8e302b5dce3
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 22:37:07 +0100

    telescope: fix keymap for project.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,18 +3,25 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
-    'DrKJeff16/project.nvim',
+    { 'nvim-telescope/telescope-project.nvim', event = 'VeryLazy' },
   },
   opts = {},
   keys = {
-    { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
-    { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = '[S]earch [K]eymaps' },
-    { '<leader>sf', '<cmd>Telescope find_files<cr>', desc = '[S]earch [F]iles' },
-    { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch current [W]ord' },
-    { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = '[S]earch by [G]rep' },
-    { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = '[S]earch [D]iagnostics' },
-    { '<leader>sr', '<cmd>Telescope resume<cr>', desc = '[S]earch [R]esume' },
-    { '<leader><leader>', '<cmd>Telescope buffers<cr>', desc = '[ ] Find existing buffers' },
-    { '<leader>sp', "<cmd>lua require'telescope'.extensions.project.project{}<cr>", desc = '[S]earch [P]roject' },
+    { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = 'Search Help' },
+    { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = 'Search Keymaps' },
+    { '<leader>sf', '<cmd>Telescope find_files<cr>', desc = 'Search Files' },
+    { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = 'Search current word' },
+    { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = 'Search by grep' },
+    { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = 'Search diagnostics' },
+    { '<leader>sr', '<cmd>Telescope resume<cr>', desc = 'Search Resume' },
+    { '<leader><leader>', '<cmd>Telescope buffers<cr>', desc = 'Find existing buffers' },
+    {
+      '<leader>sp',
+      function()
+        require('telescope').load_extension('project')
+        require('telescope').extensions.project.project({})
+      end,
+      desc = 'Search projects',
+    },
   },
 }
