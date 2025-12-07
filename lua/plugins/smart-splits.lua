41d5d2726fa8cf6b497a7494e6fcccd7f9fd0462
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 16:09:52 +0100

    smart-splits: move to keys table for config

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,16 +1,13 @@
 return {
   'mrjones2014/smart-splits.nvim',
-  config = function()
-    local keymap = vim.keymap
-
-    keymap.set('n', '<A-h>', '<cmd>SmartResizeLeft<cr>')
-    keymap.set('n', '<A-l>', '<cmd>SmartResizeRight<cr>')
-    keymap.set('n', '<A-k>', '<cmd>SmartResizeUp<cr>')
-    keymap.set('n', '<A-j>', '<cmd>SmartResizeDown<cr>')
-
-    keymap.set('n', '<C-h>', '<cmd>SmartCursorMoveLeft<cr>')
-    keymap.set('n', '<C-l>', '<cmd>SmartCursorMoveRight<cr>')
-    keymap.set('n', '<C-k>', '<cmd>SmartCursorMoveUp<cr>')
-    keymap.set('n', '<C-j>', '<cmd>SmartCursorMoveDown<cr>')
-  end,
+  keys = {
+    { '<A-h>', '<cmd>SmartResizeLeft<cr>', desc = 'Resize Left' },
+    { '<A-l>', '<cmd>SmartResizeRight<cr>', desc = 'Resize Right' },
+    { '<A-k>', '<cmd>SmartResizeUp<cr>', desc = 'Resize Up' },
+    { '<A-j>', '<cmd>SmartResizeDown<cr>', desc = 'Resize Down' },
+    { '<C-h>', '<cmd>SmartCursorMoveLeft<cr>', desc = 'Move Left' },
+    { '<C-l>', '<cmd>SmartCursorMoveRight<cr>', desc = 'Move Right' },
+    { '<C-k>', '<cmd>SmartCursorMoveUp<cr>', desc = 'Move Up' },
+    { '<C-j>', '<cmd>SmartCursorMoveDown<cr>', desc = 'Move Down' },
+  },
 }
