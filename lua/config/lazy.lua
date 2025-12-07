3a7635149c3dded5dc9a207eb3c83acfa14b19e7
Moritz Waldleben <moritz@waldleben.ch>
2025-12-07 21:33:10 +0100

    lsp: fix import for lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = {
     { import = 'plugins' },
+    { import = 'plugins.lsp' },
   },
   install = { colorscheme = { 'catppuccin' } },
   checker = {
